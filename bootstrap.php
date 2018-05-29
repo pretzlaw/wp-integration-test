cd3c8d6ef9be055424879cd077fb4e816408fd6c
Pretzlaw <mail@mike-pretzlaw.de>
2018-05-29 08:15:24 +0200

    Recover globals due to phpUnit problem

--- bootstrap.php
+++ bootstrap.php
@@ -5,3 +5,11 @@ require_once 'vendor/autoload.php';
 const WP_DEBUG = 1;
 
 \Pretzlaw\WPInt\run_wp();
+
+foreach ( \get_defined_vars() as $name => $value ) {
+	if ( ! isset( $GLOBALS[ $name ] ) ) {
+		continue;
+	}
+
+	$$name = $GLOBALS[ $name ];
+}
