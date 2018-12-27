e54b5a499808eab33855ab9f6f64eef2bacd8a68
Pretzlaw <mail+99@mike-pretzlaw.de>
2018-12-27 20:11:35 +0100

    PHP: Compat with PHP 7.3 by suppressing notices
    WP 5.0 has no notices in PHP 7.3 but older versions do.
    As notices are not that important we decided for now to suppress
    notices and deprecation warnings.
    Previously PHPUnit turned them into exceptions.
    
    * phpunit.xml.dist suppress notice and deprecations
    * docker-compose.yml: Add PHP 7.3 env for testing
    * PostAssertions.php: Gather clutter to safely remove after failed tests

--- lib/Pretzlaw/WPInt/Traits/PostAssertions.php
+++ lib/Pretzlaw/WPInt/Traits/PostAssertions.php
@@ -4,17 +4,32 @@
 namespace Pretzlaw\WPInt\Traits;
 
 
+use Pretzlaw\WPInt\Mocks\ExpectedFilter;
 use Pretzlaw\WPInt\Mocks\Post\ExpectWpInsertPost;
 
 trait PostAssertions {
 	use PostQueryAssertions;
+
+	private $wpPostClutter = [];
 
 	protected function expectWpPostCreationWithSubset( $expectedSubset ) {
         $mockObject = new ExpectWpInsertPost($expectedSubset);
 
         $mockObject->expects($this->atLeastOnce());
         $this->registerMockObject($mockObject);
+        $this->wpPostClutter[] = $mockObject;
 
         $mockObject->addFilter();
 	}
+
+    /**
+     * @after
+     */
+	protected function tearDownPostClutter() {
+        foreach ($this->wpPostClutter as $clutter) {
+            if ($clutter instanceof ExpectedFilter) {
+                $clutter->removeFilter();
+            }
+        }
+    }
 }
