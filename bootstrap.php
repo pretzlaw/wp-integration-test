49134e29686845046ac89cd880ed516b94dc4abe
rmp-up <mail+99@rmp-up.de>
2021-01-09 02:35:14 +0100

    qa: fix PHPUnit problems occurring on TravisCI

--- bootstrap.php
+++ bootstrap.php
@@ -6,6 +6,11 @@ const WP_DEBUG = 1;
 
 \Pretzlaw\WPInt\run_wp();
 
+/**
+ * Fixing PHPUnit stuff
+ */
+
+// Globals sometimes removed before bootstrap
 foreach (\get_defined_vars() as $name => $value) {
     if (!isset($GLOBALS[$name])) {
         continue;
@@ -13,6 +18,10 @@ foreach (\get_defined_vars() as $name => $value) {
 
     $$name = $GLOBALS[$name];
 }
+
+// Fatal error: Uncaught TypeError: date() expects parameter 2 to be int
+// see vendor/phpunit/php-code-coverage/src/Report/Html/Facade.php
+$_SERVER['REQUEST_TIME'] = \time();
 
 /**
  * Doubles for testing
